738130adcc3d9886d0a4b8d752565e19a8f7e348
habteyonas <39593593+habteyonas@users.noreply.github.com>
2026-01-20 09:11:56 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,9 +1,1 @@
-import Gemini from "Gemini 2.0 Flash";
-const client = new  Gemini();
 
-const response = await client.responses.create({
-  model: "Gemini 2.0 Flash",
-  input: "Write a short bedtime story about a unicorn.",
-});
-
-console.log(response.output_text);
